28c3e1210acda0d6d3e41de2f75ce1bbe3561d2e
Andres Ossa <aossa@sonatype.com>
2019-01-08 18:21:41 -0500

    Read predictions and hashtags from json files

--- bot.js
+++ bot.js
@@ -2,28 +2,8 @@ require('dotenv').config();
 
 const twit = require('twit');
 const config = require('./config.js');
-const predictions = [
-  'Todo será horrible',
-  'El futuro no es mejor',
-  'Este platanal seguirá siendo este platanal',
-  'Ese político horrible morirá, sólo para ser reemplazado por uno peor',
-  'Todo lo que amas decaerá, te dejará, morirá, o todo lo anterior',
-  'La llama de la pasión se extinguirá, tarde o temprano',
-  'Criollombia seguirá siendo Criollombia',
-  'Uribe no vivirá por siempre, pero su legado vivirá mucho más que tú',
-  'Millones han muerto; billones morirán'
-];
-const hashtags = [
-  '#ChaosMagick',
-  '#CyberMagick',
-  '#DemonMagick',
-  '#Technomancy',
-  '#Technomancer',
-  '#Anubis',
-  '#Ayperos',
-  '#Divination',
-  '#Predictions'
-];
+const predictions = require('./predictions.json');
+const hashtags = require('./hashtags.json');
 const Twitter = new twit(config);
 
 async function tweet(status) {
