369742365408b9f8da32d9f06755f6056fe77c69
Andres Ossa <aossa@sonatype.com>
2019-01-08 16:59:12 -0500

    First simple implementation

---
+++ bot.js
@@ -0,0 +1,37 @@
+const twit = require('twit');
+const config = require('./config.js');
+const predictions = [
+  'Todo será horrible',
+  'El futuro no es mejor',
+  'Este platanal seguirá siendo este platanal',
+  'Ese político horrible morirá, sólo para ser reemplazado por uno peor',
+  'Todo lo que amas decaerá, te dejará, morirá, o todo lo anterior',
+  'La llama de la pasión se extinguirá, tarde o temprano',
+  'Criollombia seguirá siendo Criollombia',
+  'Uribe no vivirá por siempre, pero su legado vivirá mucho más que tú'
+];
+const Twitter = new twit(config);
+
+function tweet(status) {
+  return Twitter.post('statuses/update', {status: status})
+    .then(({data}) => {
+      console.info(data)
+    })
+    .catch(err => {
+      console.error(err)
+    });
+}
+
+function getPrediction() {
+  const timestamp = Date.now();
+  const random = timestamp % predictions.length;
+  const prediction = predictions[random];
+  return prediction;
+}
+
+function run() {
+  tweet(getPrediction());
+}
+
+run();
+setInterval(run, 3000000);
---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+module.exports = {
+  consumer_key: 'myn8YGEWa4nYb5qR7wIV9vcdl',  
+  consumer_secret: 'GlX99WwTu2s7fOpxYViFBK9iBfzqJBhuMP92QrNyOLpIPmpda5',
+  access_token: '1082732836606668800-U57rwLKKpCWmSJc9THMhBPTU0dgzeP',  
+  access_token_secret: 'hmf26e8HKGCDS4eHxoGRU6UIxAoytCYFHd7XRlFnSdKjv'
+}
