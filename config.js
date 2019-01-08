bb7a4d78cd786f965229dac36e22d05d8005429a
Andres Ossa <aossa@sonatype.com>
2019-01-08 17:19:01 -0500

    Obscure keys

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
  */
 
 module.exports = {
-  consumer_key: 'myn8YGEWa4nYb5qR7wIV9vcdl',  
-  consumer_secret: 'GlX99WwTu2s7fOpxYViFBK9iBfzqJBhuMP92QrNyOLpIPmpda5',
-  access_token: '1082732836606668800-U57rwLKKpCWmSJc9THMhBPTU0dgzeP',  
-  access_token_secret: 'hmf26e8HKGCDS4eHxoGRU6UIxAoytCYFHd7XRlFnSdKjv'
+  consumer_key: '8y4983492347',  
+  consumer_secret: 'dfdsfdsafadsfadsf',
+  access_token: '2344234234234-U57rwLKKpCWmSJc9THMhBPTU0dgzeP',  
+  access_token_secret: '23423423423dgdfsgdfg'
 }
