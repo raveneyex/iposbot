aa343db4d11794484d9dfcb28dd4a1b630a6f68d
Andres Ossa <aossa@sonatype.com>
2019-01-08 18:27:12 -0500

    Modularize

--- config.js
+++ config.js
@@ -10,5 +10,6 @@ module.exports = {
   consumer_key: process.env.API_KEY,  
   consumer_secret: process.env.API_SECRET,
   access_token: process.env.TOKEN,
-  access_token_secret: process.env.TOKEN_SECRET
+  access_token_secret: process.env.TOKEN_SECRET,
+  interval: 3000000
 };
